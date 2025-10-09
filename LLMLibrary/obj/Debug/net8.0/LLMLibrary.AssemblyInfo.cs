f4dd45804f9276d5e02c731b7b2f1b2bed408632
Jellywase <jellywase@naver.com>
2025-10-10 00:49:45 +0900

    d

--- LLMLibrary/obj/Debug/net8.0/LLMLibrary.AssemblyInfo.cs
+++ LLMLibrary/obj/Debug/net8.0/LLMLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LLMLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6653c886e20ace10b55fe275bbec4622ba0b6a70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4b53af89ffe34d1e679d27c19e0e79372ec01e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LLMLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LLMLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
